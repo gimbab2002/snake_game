4e0d7956d105a80d6ff08b1778758e81e98eacaf
YangHeeChang <diogmlckd@gmail.com>
2021-11-17 07:36:21 +0900

    뱀의 움직임 구현 중

--- main.c
+++ main.c
@@ -7,12 +7,12 @@ void make_stage();                  //스테이지 구현 함수
 int getCommand();                   //키보드 입력 함수
 void gameover();                    //게임오버 화면
 void startscr();                    //시작 화면
+void snake_move();                  //뱀의 움직임
 
 int main(void) {
 	startscr();
-
-	char input;
 	make_stage();
+	snake_move();
 	return 0;
 }
 
@@ -58,4 +58,60 @@ void gameover() {
 	printf("        *  ***    *****     *  *  *  *   ******           *    *    *   *    ******   *****         ");
 	printf("        *    *   *     *   *     *    *  *                *    *     * *     *        *    *        ");
 	printf("        ******  *       *  *     *    *  ******            ****       *      ******   *     *      \n");
+}
+
+void snake_move() {
+	int x = 9, y = 9;
+	char dir = 'a';
+	char input;
+	gotoxy(x, y);
+	printf("*");
+	while (1) {
+		getCommand();
+		input = getCommand();
+		if (input == 'w') {
+			if (dir != 's') {
+				y = y - 1;
+				gotoxy(x, y);
+				printf("*");
+				dir = 'w';
+			}
+			else
+				continue;
+		}
+		if (input == 'a') {
+			if (dir != 'd') {
+				x = x - 1;
+				gotoxy(x, y);
+				printf("*");
+				dir = 'a';
+			}
+			else
+				continue;
+		}
+		if (input == 's') {
+			if (dir != 'w') {
+				y = y + 1;
+				gotoxy(x, y);
+				printf("*");
+				dir = 's';
+			}
+			else
+				continue;
+		}
+		if (input == 'd') {
+			if (dir != 'a') {
+				x = x + 1;
+				gotoxy(x, y);
+				printf("*");
+				dir = 'd';
+			}
+			else
+				continue;
+		}
+		if (input == 'p') {
+			gameover();
+			break;
+		}
+	}
 }
