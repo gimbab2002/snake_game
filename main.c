7df36db00fbf2d2123b2c2d4773578e4fd5782be
temraire117 <temraire117@gmail.com>
2021-11-16 19:14:34 +0900

    add title, move stage
    add 'SNAKE GAME' title on top
    move stage(make_stage()) to middle of console

--- main.c
+++ main.c
@@ -6,14 +6,18 @@ void gotoxy(int x, int y);          //입력 위치 설정 함수
 void make_stage();                  //스테이지 구현 함수
 int getCommand();                   //키보드 입력 함수
 
+void startscr();                    //시작 화면 
+
 int main(void) {
+	startscr();
+
 	char input;
 	make_stage();
 	return 0;
 }
 
 void gotoxy(int x, int y) {
-	COORD pos = { x * 2, y };
+	COORD pos = { 30 + x * 2, 10 + y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
@@ -36,3 +40,13 @@ int getCommand() {
 	}
 	return -1;
 };
+
+void startscr()
+{
+	system("mode con cols=100 lines=40");
+	printf("   ******   **    *       *      *    *  ******           ******      *        **    **    ******   ");
+	printf("   *        * *   *      * *     *   *   *                *          * *      *  *  *  *   *        ");
+	printf("   ******   *  *  *     *****    ****    ******           *  ***    *****     *  *  *  *   ******   ");
+	printf("        *   *   * *    *     *   *   *   *                *    *   *     *   *     *    *  *        ");
+	printf("   ******   *    **   *       *  *    *  ******           ******  *       *  *     *    *  ******  \n");
+}
